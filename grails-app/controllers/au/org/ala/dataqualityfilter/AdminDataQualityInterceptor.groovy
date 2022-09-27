837d07566565717a58f88802447071f1b773e27d
Sushant <sushant.ghimire@csiro.au>
2022-09-28 09:44:41 +1000

    issue #6 adding openapi info config to application.yml, updating to checks for oidc.enabled status for config bypass

--- grails-app/controllers/au/org/ala/dataqualityfilter/AdminDataQualityInterceptor.groovy
+++ grails-app/controllers/au/org/ala/dataqualityfilter/AdminDataQualityInterceptor.groovy
@@ -22,11 +22,8 @@ class AdminDataQualityInterceptor {
 
     boolean before() {
 
-        if (!grailsApplication.config.getProperty("security.cas.casServerName") && grailsApplication.config.getProperty('security.cas.bypass', Boolean, false)) {
+        if (!grailsApplication.config.security.oidc.enabled.toBoolean()) {
             // Standard Grails config - bypass
-            true
-        } else if (!grailsApplication.config.getProperty("casServerName") && grailsApplication.config.getProperty("disableCAS", Boolean, false)) {
-            // old-style AUTH config - bypass
             true
         } else if (!authService?.userInRole(grailsApplication.config.getProperty("auth.admin_role", String, "ROLE_ADMIN"))) {
             log.debug "User not authorised to access the page: ${params.controller}/${params.action?:''}. Redirecting to index."
